a83476433f2c2d00e2df7df74596f25e156c8225
daniel <partidapd@gmail.com>
2023-01-26 10:10:36 +0100

    Declare basic function to call onclick

--- startbootstrap-creative-gh-pages/js/scripts.js
+++ startbootstrap-creative-gh-pages/js/scripts.js
@@ -56,4 +56,8 @@ window.addEventListener('DOMContentLoaded', event => {
         elements: '#portfolio a.portfolio-box'
     });
 
+    onclick = () => {
+        document.getElementById("letter").innerHTML = "Hello World";
+    };
+
 });
